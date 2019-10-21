c7fbc940ea0add9aebc5e0fda1e1464506487a83
Shadowborn <james.starks023@gmail.com>
2019-10-20 22:49:11 -0600

    Udemy React

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+// ^ this is taking the document and replacing the elment with the id of root in our html file, with our function App
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
